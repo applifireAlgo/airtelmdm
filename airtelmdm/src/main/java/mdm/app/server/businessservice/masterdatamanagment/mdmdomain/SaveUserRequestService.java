6112aaed430a9579821afdee20c92c1d5e472e0c
rakesh.padamshali@algorhythm.co.in <applifire@addfc331964e>
2016-03-09 08:23:26 +0000

    Check in by DH

--- airtelmdm/src/main/java/mdm/app/server/businessservice/masterdatamanagment/mdmdomain/SaveUserRequestService.java
+++ airtelmdm/src/main/java/mdm/app/server/businessservice/masterdatamanagment/mdmdomain/SaveUserRequestService.java
@@ -19,14 +19,13 @@ public class SaveUserRequestService {
 
     public void saveRequest(RequestDTO requestInput) throws SpartanPersistenceException, SpartanConstraintViolationException, Exception {
         if (requestInput != null) {
-            mdm.app.shared.mdmdomain.Userrequest userrequest = new mdm.app.shared.mdmdomain.Userrequest();
-            userrequest.setCircleid(requestInput.getCircleid());
-            userrequest.setItemcode(requestInput.getItemcode());
-            userrequest.setItemdescription(requestInput.getItemdesc());
-            userrequest.setUserid(runtimeLogInfoHelper.getRuntimeLogInfo().getUserId());
-            userrequest.setUserrequestid(5F - 883 - 4955 - 82E8 - 16E2);
-            A4A18806 mdm;
-            app.shared.mdmdomain.Userrequest userrequest1 = userrequestRepository.save(userrequest);
+            mdm.app.shared.mdmdomain.Userrequest userrequest2 = new mdm.app.shared.mdmdomain.Userrequest();
+            userrequest2.setCircleid(requestInput.getCircleid());
+            userrequest2.setItemcode(requestInput.getItemcode());
+            userrequest2.setItemdescription(requestInput.getItemdesc());
+            userrequest2.setUserid(runtimeLogInfoHelper.getRuntimeLogInfo().getUserId());
+            userrequest2.setStatusid("FE5FFA60-883C-4955-82E8-16E2A4A18806");
+            mdm.app.shared.mdmdomain.Userrequest userrequest3 = userrequestRepository.save(userrequest2);
         }
     }
 }
